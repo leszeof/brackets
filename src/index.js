67b191bca87a0388cd5210edd734d2283e38f5c9
leszeof <literature198@gmail.com>
2020-10-18 19:33:00 +0400

    add: solve task brackets

--- src/index.js
+++ src/index.js
@@ -49,21 +49,3 @@ module.exports = function check(str, bracketsConfig) {
 
 
 }
-
-
-
-
-check('()',         [['(', ')']]) // -> true
-check('((()))()',   [['(', ')']]) // -> true
-check('())(',       [['(', ')']]) // -> false
-check('([{}])',     [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
-check('[(])',       [['(', ')'], ['[', ']']]) // -> false
-check('[]()',       [['(', ')'], ['[', ']']]) // -> true
-check('[]()(',      [['(', ')'], ['[', ']']]) // -> false
-
-// special case: opening and closing bracket can be the same :)
-
-check('||',           [['|', '|']]) // -> true
-check('|()|',         [['(', ')'], ['|', '|']]) // -> true
-check('|(|)',         [['(', ')'], ['|', '|']]) // -> false
-check('|()|(||)||',   [['(', ')'], ['|', '|']]) // -> true
